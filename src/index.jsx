795233aecdc184dd60314cb6c4f59a9ca84bb620
RussHR <russ.rinzler@gmail.com>
2015-09-07 18:57:13 -0700

    will search for and attach images

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,10 @@ superagentJSONP(superagent);
 
 class TrendtrendApp extends React.Component {
 
+    state ={
+        imageSrcs: []
+    };
+
     searchByTag(e) {
         e.preventDefault();
         let tag = React.findDOMNode(this.refs.tag).value.trim();
@@ -32,15 +36,29 @@ class TrendtrendApp extends React.Component {
                         if (imageSrcs.length >= 20) break;
                     }
                 }
+                self.setState({ imageSrcs: imageSrcs });
             });
     }
 
+    loadImages() {
+        let images = this.state.imageSrcs.map((imageSrc) => {
+            return (
+                <img src={imageSrc} onLoad={ () => {console.log('loaded')} } />
+            );
+        });
+        return images;
+    }
+
     render() {
+        let images = this.state.imageSrcs.length > 0 ? this.loadImages() : null;
         return (
-            <form className="commentForm" onSubmit={ ::this.searchByTag }>
-                <input type="text" placeholder="Tag to search" ref="tag" />
-                <input type="submit" value="Post" />
-            </form>
+            <div>
+                <form className="commentForm" onSubmit={ ::this.searchByTag }>
+                    <input type="text" placeholder="Tag to search" ref="tag" />
+                    <input type="submit" value="Search Posts" />
+                </form>
+                { images }
+            </div>
         );
     }
 }
