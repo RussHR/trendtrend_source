abe14894dd7c0dc76ba4d0ade425ad475cfcd975
RussHR <russ.rinzler@gmail.com>
2015-09-12 20:35:32 -0700

    no longer combining reducers

--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -1,20 +1,28 @@
 import { combineReducers } from 'redux';
 import * as types          from '../constants/ActionTypes';
 
-function appPhase(state = 'request tag', action) {
+const initialState = {
+    appPhase: 'request tag'
+}
+export default function appPhase(state = initialState, action) {
     switch (action.type) {
-        case types.REQUEST_TAG:
-            return 'request tag'
-        case types.FIND_ASSETS:
-            return 'find assets'
-        case types.LOAD_ASSETS:
-            return 'load assets'
-        case types.PLAY_ANIMATION:
-            return 'play animation'
+        case types.REQUEST_TAG: 
+            return {
+                appPhase: 'request tag'
+            }
+        case types.FIND_ASSETS: 
+            return {
+                appPhase: 'find assets'
+            }
+        case types.LOAD_ASSETS: 
+            return {
+                appPhase: 'load assets'
+            }
+        case types.PLAY_ANIMATION: 
+            return {
+                appPhase: 'play animation'
+            }
         default:
             return state;
     }
 }
-
-const mainReducer = combineReducers({ appPhase });
-export default mainReducer;
