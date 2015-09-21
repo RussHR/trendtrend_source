1f9226b485682ab43994e1634d86d96add277555
RussHR <russ.rinzler@gmail.com>
2015-09-20 22:15:08 -0700

    minor cleaning, found that I can't import routes for some reason

--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,18 @@ import React, { Component }             from 'react';
 import { createStore, applyMiddleware } from 'redux';  
 import { Provider }                     from 'react-redux';
 import thunk                            from 'redux-thunk';
-import TrendtrendApp                    from './TrendtrendApp';  
-import mainReducer                      from './reducers';
-import {Router, Route, IndexRoute } from 'react-router';
-import createHistory from 'history/lib/createBrowserHistory';
-import FindAssetsSection                    from './components/FindAssetsSection';  
+import { Router, Route, IndexRoute }    from 'react-router';
+import createHistory                    from 'history/lib/createBrowserHistory';
 
+import mainReducer                      from './reducers';
+
+import TrendtrendApp                    from './TrendtrendApp';  
+import FindAssetsSection                from './components/FindAssetsSection';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const store = createStoreWithMiddleware(mainReducer);
 
+// routes are here because I get an error when trying to import
 const routes = (
     <Route path="/" component={TrendtrendApp}>
         <IndexRoute component={FindAssetsSection} />
@@ -19,7 +21,7 @@ const routes = (
     </Route>
 );
 
-export default class App extends Component {  
+export default class Root extends Component {
     render() {
         return (
             <Provider store={store}>
@@ -29,4 +31,4 @@ export default class App extends Component {
     }
 }
 
-React.render(<App />, document.getElementById('content'));
+React.render(<Root />, document.getElementById('content'));
