d65fe37a2f11d6befaa9d8fb7e8d4950a50c2a90
RussHR <russ.rinzler@gmail.com>
2015-09-20 21:39:19 -0700

    finally got a router working

--- src/index.jsx
+++ src/index.jsx
@@ -4,20 +4,29 @@ import { Provider }                     from 'react-redux';
 import thunk                            from 'redux-thunk';
 import TrendtrendApp                    from './TrendtrendApp';  
 import mainReducer                      from './reducers';
+import {Router, Route, IndexRoute } from 'react-router';
+import createHistory from 'history/lib/createBrowserHistory';
+import FindAssetsSection                    from './components/FindAssetsSection';  
+
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const store = createStoreWithMiddleware(mainReducer);
 
+const routes = (
+    <Route path="/" component={TrendtrendApp}>
+        <IndexRoute component={FindAssetsSection} />
+        <Route path="/hm" component={FindAssetsSection} />
+    </Route>
+);
+
 export default class App extends Component {  
     render() {
         return (
-            <div id="content">
-                <Provider store={store}>
-                    { () => <TrendtrendApp /> }
-                </Provider>
-            </div>
+            <Provider store={store}>
+                { () => <Router history={createHistory()} routes={routes} /> }
+            </Provider>
         );
     }
 }
 
-React.render(<App />, document.body);
+React.render(<App />, document.getElementById('content'));
