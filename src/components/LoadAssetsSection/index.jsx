aa3079408db25ee06e9b7303061ba82c5bbee74f
RussHR <russ.rinzler@gmail.com>
2015-10-15 22:27:25 -0700

    uses the ugliest code ever to play music and console.log the max amplitude, will I regret this in the morning?

--- src/components/LoadAssetsSection/index.jsx
+++ src/components/LoadAssetsSection/index.jsx
@@ -34,74 +34,176 @@ export default class LoadAssetsSection extends Component {
     }
 
     componentDidMount() {
-        const ContextClass = (window.AudioContext || window.webkitAudioContext);
-        const audioContext = new ContextClass();
+        // create the nodes/elements
+        const AudioContextClass = (window.AudioContext || window.webkitAudioContext || window.mozAudioContext);
+        const requestAnimationFrameFunction = (window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozAnimationFrame);
+        this.audioContext = new AudioContextClass();
+        let audioBuffer;
+        this.sourceNode = this.audioContext.createBufferSource();
+        const analyserNode = this.audioContext.createAnalyser();
+        const sampleSize = 1024;
+        const javascriptNode = this.audioContext.createScriptProcessor(sampleSize, 1, 1);
+        this.audioPlaying = false;
+        window.amplitudeArray = new Uint8Array(analyserNode.frequencyBinCount);
+        window._drawTimeDomain = this._drawTimeDomain;
+        const audioUrl = this.props.tracks[0].preview_url;
+
+        // connect things
+        this.sourceNode.connect(this.audioContext.destination);
+        this.sourceNode.connect(analyserNode);
+        analyserNode.connect(javascriptNode);
+        javascriptNode.connect(this.audioContext.destination);
+
+        // setup the handler
+        javascriptNode.onaudioprocess = () => {
+            // XXX: do I need to redefine amplitude Array in here?
+            analyserNode.getByteTimeDomainData(window.amplitudeArray);
+
+            if (this.audioPlaying === true) {
+                requestAnimationFrameFunction(window._drawTimeDomain);
+            }
+        };
+
+        if (!this.audioData) {
+            this._loadSound(audioUrl);
+        } else {
+            this._playSound(audioData);
+        }
+        // const audioContext = new this.ContextClass();
+        // const request = new XMLHttpRequest();
+        // request.open('GET', this.props.tracks[0].preview_url, true)
+        // request.responseType = 'arraybuffer';
+        // request.send();
+        // request.onload = (e) => {
+        //     audioContext.decodeAudioData(request.response, (buffer) => {
+        //         this._processBuffer(buffer);
+        //     });
+        // };
+    }
+
+    _loadSound(url) {
         const request = new XMLHttpRequest();
-        request.open('GET', this.props.tracks[0].preview_url, true)
+        request.open('GET', url, true)
         request.responseType = 'arraybuffer';
-        request.send();
         request.onload = (e) => {
-            audioContext.decodeAudioData(request.response, (buffer) => {
-                this._runBufferThroughLowpass(buffer);
+            this.audioContext.decodeAudioData(request.response, (buffer) => {
+                this.audioData = buffer;
+                this._playSound(this.audioData);
             });
         };
+        request.send();
     }
 
-    _runBufferThroughLowpass(buffer) {
-        const offlineContext = new OfflineAudioContext(1, buffer.length, buffer.sampleRate);
-        const source = offlineContext.createBufferSource();
-        source.buffer = buffer;
-        const filter = offlineContext.createBiquadFilter();
-        filter.type = "lowpass";
-        source.connect(filter);
-        filter.connect(offlineContext.destination);
-        source.start(0);
-        offlineContext.startRendering();
-        offlineContext.oncomplete = (e) => {
-            this._processBuffer(e.renderedBuffer);
-        };
+    _playSound(buffer) {
+        this.sourceNode.buffer = buffer;
+        this.sourceNode.start(0);
+        this.sourceNode.loop = true;
+        this.audioPlaying = true;
     }
-    _processBuffer(renderedBuffer) {
-        const float32Array = renderedBuffer.getChannelData(0);
-        const maxValue = this._getFloat32ArrayMax(float32Array);
-        const minValue = this._getFloat32ArrayMin(float32Array);
-        const threshold = minValue + (maxValue - minValue) * 0.92;
-        const peaks = this._getPeaksAtThreshold(float32Array, threshold);
-        console.log(peaks);
-    }
-    // this requires a custom function because Math.max/min is recursive and causes max stack err
-    _getFloat32ArrayMax(float32Array) {
-        let maxValue = -Infinity;
-        const arrLength = float32Array.length;
-
-        for (let i = 0; i < arrLength; i++) {
-            const currentValue = float32Array[i];
-            if (maxValue < currentValue) maxValue = currentValue;
-        }
-        return maxValue;
-    }
-    _getFloat32ArrayMin(float32Array) {
+    _drawTimeDomain() {
+        window.requestAnimationFrame(window._drawTimeDomain);
+        window.frameCount = (window.frameCount || 0);
         let minValue = Infinity;
-        const arrLength = float32Array.length;
-
-        for (let i = 0; i < arrLength; i++) {
-            const currentValue = float32Array[i];
-            if (minValue > currentValue) minValue = currentValue;
-        }
-        return minValue;
-    }
-    _getPeaksAtThreshold(float32Array, threshold) {
-        const peaks = [];
-        const arrLength = float32Array.length;
-
-        for (let i=0; i < arrLength; i++) {
-            if (float32Array[i] > threshold) {
-                peaks.push(i);
-                i += 10000;
+        let maxValue = -Infinity;
+        for (let i = 0; i < window.amplitudeArray.length; i++) {
+            const value = window.amplitudeArray[i] / 255;
+            if (value > maxValue) {
+                maxValue = value;
+            } else if (value < minValue) {
+                minValue = value;
             }
         }
-        return peaks;
+        window.frameCount += 1;
+        if (window.frameCount > 200) {
+            console.log(maxValue);
+            window.frameCount = 0;
+        }
     }
+    // _processBuffer(buffer) {
+    //     let audioContext;
+    //     let audioBuffer;
+    //     let sourceNode;
+    //     let analyserNode;
+    //     let javascriptNode;
+    //     const audioData = null;
+    //     const audioPlaying = false;
+    //     const sampleSize = 1024;
+    //     // context { source -> filter -> analyser -> context.destination }
+    //     const offlineContext = new OfflineAudioContext(1, buffer.length, buffer.sampleRate);
+    //     const source = offlineContext.createBufferSource();
+    //     source.buffer = buffer;
+    //     const filter = offlineContext.createBiquadFilter();
+    //     filter.type = "lowpass";
+    //     const analyser = offlineContext.createAnalyser();
+    //     const dataArray = new Uint8Array(analyser.fftSize);
+    //     debugger
+    //     source.connect(filter);
+    //     filter.connect(offlineContext.destination);
+    //     source.start(0);
+    //     offlineContext.startRendering();
+    //     offlineContext.oncomplete = (e) => {
+    //         this._processBuffer(e.renderedBuffer);
+    //     };
+    // }
+
+    // _processBuffer(renderedBuffer) {
+    //     const float32Array = renderedBuffer.getChannelData(0);
+    //     const maxValue = this._getFloat32ArrayMax(float32Array);
+    //     const minValue = this._getFloat32ArrayMin(float32Array);
+    //     const threshold = minValue + (maxValue - minValue) * 0.9;
+    //     const peaks = this._getPeaksAtThreshold(float32Array, threshold);
+    //     const intervals = this._getIntervalsFromPeaks(peaks);
+    // }
+
+    // this requires a custom function because Math.max/min is recursive and causes max stack err
+    // _getFloat32ArrayMax(float32Array) {
+    //     let maxValue = -Infinity;
+    //     const arrLength = float32Array.length;
+
+    //     for (let i = 0; i < arrLength; i++) {
+    //         const currentValue = float32Array[i];
+    //         if (maxValue < currentValue) maxValue = currentValue;
+    //     }
+
+    //     return maxValue;
+    // }
+
+    // _getFloat32ArrayMin(float32Array) {
+    //     let minValue = Infinity;
+    //     const arrLength = float32Array.length;
+
+    //     for (let i = 0; i < arrLength; i++) {
+    //         const currentValue = float32Array[i];
+    //         if (minValue > currentValue) minValue = currentValue;
+    //     }
+
+    //     return minValue;
+    // }
+
+    // _getPeaksAtThreshold(float32Array, threshold) {
+    //     const peaks = [];
+    //     const arrLength = float32Array.length;
+
+    //     for (let i=0; i < arrLength; i++) {
+    //         if (float32Array[i] > threshold) {
+    //             peaks.push(i);
+    //             i += 10000;
+    //         }
+    //     }
+
+    //     return peaks;
+    // }
+
+    // _getIntervalsFromPeaks(peaks) {
+    //     const intervals = [];
+    //     peaks.forEach((peak, peakIndex) => {
+    //         for (let i = 0; i < 10; i++) {
+    //             const interval = peaks[peakIndex + i] - peak;
+    //         }
+    //     });
+
+    //     return intervals;
+    // }
 
     incrementLoadedImages() {
         const { dispatch, loadedImageCount, history } = this.props;
